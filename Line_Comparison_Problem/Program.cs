309f5362adcafc5422385938651289d926f7acb5
PRAMODSHENDAGE <shendagepramod@gmail.com>
2022-05-07 15:24:48 +0530

    Add project files.

---
+++ Line_Comparison_Problem/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Line_Comparison_Problem;
+Console.WriteLine("Welcome to Line Comparison Computation Program");
+
+UC1_LineLength.LineLength();
+UC_2TwoLinesEqual.TwoLineInput();
+UC_3ComparetoMethods.TwoLineInput();
